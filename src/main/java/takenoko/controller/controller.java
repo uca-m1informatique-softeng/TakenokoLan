f05b184b3a5b832f3cc84fab494e25a9ddc3c3cc
LotfiYacine <dtriamort@gmail.com>
2019-02-21 11:52:13 +0100

    création d'un chemin /Piocher #2

--- src/main/java/takenoko/controller/controller.java
+++ src/main/java/takenoko/controller/controller.java
@@ -1,8 +1,12 @@
 package takenoko.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import takenoko.configuration.Takenoko;
+import takenoko.ressources.Parcelle;
+
+import java.util.ArrayList;
 
 @RestController
 public class controller {
@@ -16,4 +20,8 @@ public class controller {
         return "done";
     }
 
+    @GetMapping(value = "/Piocher")
+    public ArrayList<Parcelle> piocher() {
+        return takenoko.getLaPiocheParcelle().piocherParcelle();
+    }
 }
