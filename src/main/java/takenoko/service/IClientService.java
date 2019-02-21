948dd908b08bbbdb545a011adda3acb61f589c92
LotfiYacine <dtriamort@gmail.com>
2019-02-21 11:53:21 +0100

    implémentation de /Piocher sur le service client #2

--- src/main/java/takenoko/service/IClientService.java
+++ src/main/java/takenoko/service/IClientService.java
@@ -1,6 +1,10 @@
 package takenoko.service;
 
+import takenoko.ressources.Parcelle;
+
+import java.util.ArrayList;
+
 public interface IClientService {
 
-
+    ArrayList<Parcelle> piocher();
 }
--- src/main/java/takenoko/service/impl/ClientService.java
+++ src/main/java/takenoko/service/impl/ClientService.java
@@ -1,9 +1,14 @@
 package takenoko.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import takenoko.configuration.RestTemplateConfig;
+import takenoko.ressources.Parcelle;
 import takenoko.service.IClientService;
+
+import java.util.ArrayList;
 
 @Service
 public class ClientService implements IClientService {
@@ -11,5 +16,13 @@ public class ClientService implements IClientService {
     @Autowired
     private RestTemplateConfig restTemplate;
 
-
+    @Override
+    public ArrayList<Parcelle> piocher() {
+        return restTemplate.restTemplate().exchange(
+                "http://localhost:8080/Piocher",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<ArrayList<Parcelle>>() {
+                }).getBody();
+    }
 }
