7d22b3a0659217e2d505384491f04fc295a3c164
LotfiYacine <dtriamort@gmail.com>
2019-03-09 18:25:49 +0100

    Fin du scénario #5

--- src/test/java/takenoko/pioches/PiocheParcelleIsEmpty.java
+++ src/test/java/takenoko/pioches/PiocheParcelleIsEmpty.java
@@ -15,25 +15,43 @@ import static org.junit.Assert.assertThat;
 public class PiocheParcelleIsEmpty extends SpringRootTest {
     private ResponseEntity<Boolean> response; // output
 
-    @Given("^une partie")
+    @Given("^une partie$")
     public void init() {
         template.getForEntity("http://localhost:8080/init", String.class);
     }
 
     @When("^le client appelle /PiocheParcelleIsEmpty$")
-    public void client_GET_PiocheParcelleVide$() {
+    public void clientGETPiocheParcelleIsEmpty() {
         response = template.getForEntity("http://localhost:8080/PiocheParcelleIsEmpty", Boolean.class);
     }
 
     @Then("^le client reçoit status code (\\d+)$")
-    public void client_recoit_status_code(int statusCode) {
+    public void clientRecoitStatusCode(int statusCode) {
         HttpStatus currentStatusCode = response.getStatusCode();
 
         assertThat("status code is incorrect : " + response.getBody(), currentStatusCode.value(), is(statusCode));
     }
 
-    @And("^le client reçoit faux")
-    public void client_recoit_faux() {
+    @And("^le client reçoit faux$")
+    public void clientRecoitFaux() {
         assertEquals(response.getBody(), false);
+    }
+
+    @Given("^la pioche est vide$")
+    public void videPioche() {
+        //on vide la pioche
+        for(int i=0;i<9;i++) {
+            template.getForEntity("http://localhost:8080/Piocher", String.class);
+        }
+    }
+
+    @When("^le client appelle /PiocheParcelleIsEmpty alors que la pioche est vide$")
+    public void clientGETPiocheParcelleIsEmpty2() {
+        response = template.getForEntity("http://localhost:8080/PiocheParcelleIsEmpty", Boolean.class);
+    }
+
+    @Then("^le client reçoit vrai$")
+    public void clientRecoitVrai() {
+        assertEquals(response.getBody(), true);
     }
 }
