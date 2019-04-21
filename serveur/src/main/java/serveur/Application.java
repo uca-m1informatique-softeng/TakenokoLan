ac40c17e517a861f3112a6eeed4038b47f3ad262
ChloeMaccarinelli <chloe.maccarinelli@unice.fr>
2019-04-21 21:04:19 +0200

    déployement sous docker
    création des dockerfiles et du docker compose
    modification des pom.xml pour la bonne génération
    des archives .jar.
    l'application se lance bien dans docker
    
    problème à résoudre  :
    
    pour http://192.168.99.100:8081/newPlayer:
    
    There was an unexpected error (type=Internal Server Error, status=500). I/O error on GET request for "http://localhost:8080/lDnLeVrZVm/Connect": Connection refused (Connection refused)
    
    piste de réfléxion :
     modifier automatiquement "localhost" par le host de la machine qui lance l'application que ce soit localhost ou une machine virtuelle.
    puisque "http://192.168.99.100:8080/lDnLeVrZVm/Connect" fontionne.
    
    #28

--- serveur/src/main/java/serveur/Application.java
+++ serveur/src/main/java/serveur/Application.java
@@ -3,8 +3,10 @@ package serveur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
