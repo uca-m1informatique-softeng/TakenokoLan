ebc10be1d2a8b3945f8be3bf6f2d8ae9406563b0
ChloeMaccarinelli <chloe.maccarinelli@unice.fr>
2019-04-22 13:54:33 +0200

    déployement sous docker
    correction du pom.xml de TakenokoRepository
    
    l'application tourne correctement sous docker
    
    Voir le README pour lancer l'application
    
    probleme : tourne dans docker mais ne communique pas sur le localhost !!
    
    #28

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # TakenokoLan
+
+Docker : 
+Pour lancer l'application dans docker :
+se placer dans le dossier ProjetM1 et effectuer les commandes suivantes:
+    
+        mvn clean install //crée les fichiers .jar utiles
+        # docker system prune -a -f && docker pull docker// attention supprime toutes vos images et containers déjà existant on doit recuperer l'image docker dans ce cas
+        cd serveur
+        docker build -f Dockerfile -t serveur . //crée l'image serveur
+        cd ..
+        cd joueur
+        docker build -f Dockerfile -t joueur . //crée l'image joueur
+        cd ..
+        docker-compose up 
+        
+ensuite vous allez dans votre navigateur et vous lancez l'url suivante :
+
+            http://192.168.99.100:8081/newPlayer //nouveau joueur
--- joueur/src/main/java/joueur/service/impl/ClientService.java
+++ joueur/src/main/java/joueur/service/impl/ClientService.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 @Service
 public class ClientService implements IClientService {
-    private static String REST_SERVICE_URI = "http://localhost:8080";
+    private static String REST_SERVICE_URI = "http://192.168.99.100:8080";
     private RestTemplate restTemplate = new RestTemplate();
 
     @Override
@@ -247,11 +247,11 @@ public class ClientService implements IClientService {
     public int connect(String name) {
         int[] tab =
          restTemplate.exchange(
-                 "http://localhost:8080" +"/"+name+ "/Connect",
+                 "http://192.168.99.100:8080" +"/"+name+ "/Connect",
                 HttpMethod.GET,
                 null,
                 int[].class).getBody();
-        REST_SERVICE_URI = "http://localhost:8080"+"/"+tab[0];
+        REST_SERVICE_URI = "http://192.168.99.100:8080"+"/"+tab[0];
         return tab[1];
     }
 
