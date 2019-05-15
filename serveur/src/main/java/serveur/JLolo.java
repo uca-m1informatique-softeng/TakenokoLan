c48346615576abe0df1335715aa0f8fbcaaa5550
LotfiYacine <dtriamort@gmail.com>
2019-05-15 13:07:15 +0200

    test coter serveur

--- serveur/src/main/java/serveur/JLolo.java
+++ serveur/src/main/java/serveur/JLolo.java
@@ -2,7 +2,9 @@ package serveur;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.InetAddress;
 
@@ -15,5 +17,21 @@ public class JLolo implements ApplicationListener<ApplicationReadyEvent> {
         } catch (Exception e) {
         }
         System.out.println("loopback" + InetAddress.getLoopbackAddress().getHostAddress());
+        boolean alive;
+        RestTemplate restTemplate = new RestTemplate();
+        do {
+            try {
+                restTemplate.exchange("http://" + "172.18.0.2" + ":" + "8080" + "/alive", HttpMethod.GET, null, String.class);
+                alive = true;
+            } catch (Exception e) {
+                System.out.println("Serveur inaccessible");
+                alive = false;
+                //Pour attendre 10s
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException a) {
+                }
+            }
+        } while (!alive);
     }
 }
